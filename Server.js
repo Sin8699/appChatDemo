368b5bdf062353af5f902a82e7350fe3fc314e0c
Phuong <1760393@student.hcmus.edu.vn>
2019-08-26 19:48:18 +0700

    add path for server

--- Server.js
+++ Server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 const Port = process.env.PORT || 1212;
-
+const path = require("path");
 var app = express();
 
 var server = http.Server(app);
